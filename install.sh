52b9c43f1d868972b11f533616f303f6fb8bd99c
streitlua <streitlua@gmail.com>
2017-08-10 16:59:31 +0200

    Ajout de virtualenv dans l'installation et dans le nouveau script start.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
+virtFold=venvParser
 sudo apt-get install python3-dev python3-pip
-sudo pip3 install beautifulsoup4
+sudo apt-get install virtualenv
+virtualenv -p /usr/bin/python3 $virtFold
+source $virtFold/bin/activate
+pip3 install beautifulsoup4
